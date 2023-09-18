1d6dba2e99bd61d58e27cef31387ca6c16c4e5a8
Ben Walker <benwalker1.personal@gmail.com>
2023-09-19 09:07:34 +1200

    Imported font-awesome icons for Reply and Check on the Overview page

--- src/pages/ProgressTracker/Components/Overview.jsx
+++ src/pages/ProgressTracker/Components/Overview.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import Styles from './Overview.module.css';
+import React from "react";
+import Styles from "./Overview.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReply, faCheck } from '@fortawesome/free-solid-svg-icons';
 
-export default function 
-Overview() {
+
+export default function Overview() {
   return (
     <div className={Styles.overviewContainer}>
-        <h1>Overview</h1>
+      <div className={Styles.helpRequestsContainer}>
+        <div className={Styles.helpRequestsHeader}>
+          <h1>HELP REQUESTS</h1>
+        </div>
+        <div className={Styles.helpRequestMarkReply}>
+          <p><FontAwesomeIcon icon={faReply} />REPLY</p>
+          <p><FontAwesomeIcon icon={faCheck} />MARK AS DONE</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
