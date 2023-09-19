63d6ad3ba04edbc819f1a7a0a09c4d2055d0df5a
Ben Walker <benwalker1.personal@gmail.com>
2023-09-19 17:07:28 +1200

    New code for the indivudual student requests on Progress Tracker page. Showing each students request and date time of request made

--- src/pages/ProgressTracker/Components/Overview.jsx
+++ src/pages/ProgressTracker/Components/Overview.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import Styles from "./Overview.module.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReply, faCheck } from '@fortawesome/free-solid-svg-icons';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReply, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function Overview() {
   return (
@@ -12,8 +11,54 @@ export default function Overview() {
           <h1>HELP REQUESTS</h1>
         </div>
         <div className={Styles.helpRequestMarkReply}>
-          <p><FontAwesomeIcon icon={faReply} />REPLY</p>
-          <p><FontAwesomeIcon icon={faCheck} />MARK AS DONE</p>
+          <p>
+            <FontAwesomeIcon icon={faReply} className={Styles.faIcon} />
+            REPLY
+          </p>
+          <p>
+            <FontAwesomeIcon icon={faCheck} className={Styles.faIcon} />
+            MARK AS DONE
+          </p>
+        </div>
+        <div className={Styles.individualRequestsContainer}>
+
+          {/* ------------------- */}
+
+          <div className={Styles.individualRequests}>
+
+          <input type="checkbox" id="mCheckbox" name="myCheckbox" value="checkboxValue" />
+
+            <div className={Styles.individualStudent}>
+
+              <div>
+              <img src="" alt="" />
+              <p>Student needs help with their project</p>
+              </div>
+
+              <p>Date</p>
+
+            </div>
+
+          </div>
+
+          {/* ------------------- */}
+
+          <div className={Styles.individualRequests}>
+          <input type="checkbox" id="mCheckbox" name="myCheckbox" value="checkboxValue" />
+          </div>
+
+          <div className={Styles.individualRequests}>
+          <input type="checkbox" id="mCheckbox" name="myCheckbox" value="checkboxValue" />
+          </div>
+
+          <div className={Styles.individualRequests}>
+          <input type="checkbox" id="mCheckbox" name="myCheckbox" value="checkboxValue" />
+          </div>
+
+          <div className={Styles.individualRequests}>
+          <input type="checkbox" id="mCheckbox" name="myCheckbox" value="checkboxValue" />
+          </div>
+
         </div>
       </div>
     </div>
