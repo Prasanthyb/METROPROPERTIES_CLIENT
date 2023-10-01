54738527b784c3473365844780c3f218e54fe755
Thomas Graham <seventh.child@gmail.com>
2023-10-02 00:42:03 +1300

    Initial login modal component added; still need to add actual forms to it

--- src/common/Login/Login.jsx
+++ src/common/Login/Login.jsx
@@ -4,15 +4,55 @@ import styles from './Login.module.css';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 
+//Import images
+import esc from '../../assets/LoginSignup/esc.png';
+import studentImg from '../../assets/LoginSignup/students.png';
+import teacherImg from '../../assets/LoginSignup/teachers.png';
+
+import { useState } from 'react';
 
 export default function Login(props) {
+    //su = "sign up"
+    //We'll handle the active class on the login/signup buttons by just tracking if "sign up" is selected
+    //If it isn't, we assume log in is.
+    const [suSelected, setSuSelected] = useState(false);
+
+    function handleSuChange(newSuState) {
+        setSuSelected(newSuState);
+    }
+
+    //For testing purposes. Need to import useEffect to add this back
+    // useEffect(()=>console.log(suSelected));
 
     return (
         <Modal open={props.modalOpenState} onClose={props.closeModal}>
             <Box id={styles['loginBox']}>
-                <div id={styles['closeButton']}></div>
+                <div id={styles['closeButton']}>
+                    <img alt='Close' src={esc} onClick={props.closeModal}></img>
+                </div>
                 <div id={styles['loginContainer']}>
-                    <p>Test Text</p>
+                    <div id={styles['studentLoginDiv']}>
+                        <img alt='Students' src={studentImg}></img>
+                        {/* Going to use repeated divs here instead of components since it might end up causing more
+                            problems than it solves. */}
+                        <h1>Students</h1>
+                        <div className={`${styles.lsButtonContainer}`}>
+                            {/* Using h3 mostly because I am lazy and it has the text styling I'm after.
+                                All it does is swap out the forms so no big deal probably */}
+                            <h3 onClick={()=>handleSuChange(false)} className={`${styles.loginButton} ${styles.lsButton} ${suSelected? null : styles.active}`}>LOG IN</h3>
+                            <h3 onClick={()=>handleSuChange(true)} className={`${styles.signupButton} ${styles.lsButton} ${suSelected? styles.active : null}`}>SIGN UP</h3>
+                        </div>
+                    </div>
+                    <div id={styles['teacherLoginDiv']}>
+                        <img alt='Teachers' src={teacherImg}></img>
+                        <h1>Teachers</h1>
+                        <div className={`${styles.lsButtonContainer}`}>
+                            {/* Using h3 mostly because I am lazy and it has the text styling I'm after.
+                                All it does is swap out the forms so no big deal probably */}
+                            <h3 onClick={()=>handleSuChange(false)} className={`${styles.loginButton} ${styles.lsButton} ${suSelected? null : styles.active}`}>LOG IN</h3>
+                            <h3 onClick={()=>handleSuChange(true)} className={`${styles.signupButton} ${styles.lsButton} ${suSelected? styles.active : null}`}>SIGN UP</h3>
+                        </div>
+                    </div>
                 </div>
             </Box>
         </Modal>
