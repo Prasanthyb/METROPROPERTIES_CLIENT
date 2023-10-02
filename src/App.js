e0d3d035db2d33a47c038ed7569ef2918a81fe43
Ben Walker <benwalker1.personal@gmail.com>
2023-10-02 18:59:29 +1300

    committing latest changes to local repo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import HelpRequests from './pages/HelpRequests/HelpRequests';
 // import StudentProfiles from './pages/StudentProfiles/StudentProfiles';
 import React from 'react';
-import StudentProjects from './assets/StudentDashboard/PrasanthyComponents/StudentProjects';
+import StudentProjects from './pages/PrasanthyComponents/StudentProjects';
 
 function App() {
   return (
