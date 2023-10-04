2a06ecfc44face4d63b30e1abd47cf2aedec0526
Ben Walker <benwalker1.personal@gmail.com>
2023-10-04 18:43:17 +1300

    Added StudentProfiles function component with corresponding imports for the Student Profiles page. Also added css styles to .pageContainer and .MenuOverviewContainer classes in StudentProfiles.module.css file

--- src/pages/StudentProfiles/Styles/StudentProfiles.jsx
+++ src/pages/StudentProfiles/Styles/StudentProfiles.jsx
@@ -1,1 +1,20 @@
-/* This is a STYLES place holder page for my StudentProfiles page */
+import React from "react";
+import Menu from "./Components/Menu";
+import Overview from "./Components/Overview";
+import Styles from "./HelpRequests.module.css";
+// ####### COMMON COMPONENTS IMPORTS #######
+import StudentTeacherNavBar from "../../common/StudentTeacherNavBar";
+import Footer from "../../common/Footer";
+
+export default function StudentProfiles() {
+  return (
+    <div className={Styles.pageContainer}>
+      <StudentTeacherNavBar />
+      <div className={Styles.MenuAndOverviewContainer}>
+        <Menu />
+        <Overview />
+      </div>
+      <Footer />
+    </div>
+  );
+}
